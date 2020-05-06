5e37a6a261b0ff12a6336c728c1439237fca3a3b
Brachychiton <jbuddee96@gmail.com>
2020-05-06 23:58:23 +1000

    Comments included.

--- ClinTrialSim.py
+++ ClinTrialSim.py
@@ -2,6 +2,7 @@ import simpy
 from simpy.events import AllOf
 from random import randint
 
+""" This dictionary translates process maps into format which the chainer function can read off """
 processes = {'Chain_Type': ['Single',
                             'Simple Parallel',
                             'Single',
@@ -45,45 +46,64 @@ processes = {'Chain_Type': ['Single',
              ]
             }
 def print_process_info(name, length):
+    """ Prints event details """
     print("%s took %i" % (name, length))
     print("Current time is ", env.now)
 
 def single(env, names):
+    """ Runs a single event """
     for name in names:
+        #current generation of trip time
         length = randint(1,5)
         yield env.timeout(length)
+        #additional call to print event info
         print_process_info(name, length)
 
 def simple_parallel(env, names):
+    """ Runs n=len(names) events in parallel """
     parallel_events = [env.process(single(env, name)) for name in names]
     yield AllOf(env, parallel_events)
 
 def stage_1_sub_parallel(env, names):
+    """ Produces parallel process generator for parallel stage 1 stubs """
     parallel_events = [env.process(stage_1_sub_stub(env, name)) for name in names]
     yield AllOf(env, parallel_events)
 
 def stage_1_sub_stub(env, names):
+    """ Specific event run setup for the flow diagram stub in stage 1 sub """
+    #iterating through the different events
     for name in names:
+        #if there isn't another nested list
         if isinstance(name, str):
-            print(name)
+            #then it's just a single event run
             yield env.process(single(env, [name]))
+        #otherwise there's a single nested list
         else:
-            print(type(name))
+            #run the parallel event function
             yield env.process(simple_parallel(env, name))
 
 
 def chainer(env):
+    """ Chains together all the separate process to operate the whole simulation.
+    Goes off the 'processes' dict at top of file. """
+    #iterating through chain type
     for i, chain_type in enumerate(processes['Chain_Type']):
+        #get relevant process name or nested list
         names = processes['Names'][i]
+        #basing process function choice on chain type
         if chain_type == 'Single':
             yield env.process(single(env, names))
         elif chain_type == 'Simple Parallel':
             yield env.process(simple_parallel(env, names))
         elif chain_type == 'Stage 1 Parallel':
             yield env.process(stage_1_sub_parallel(env, names))
+        #inluding else for completeness lol
         else:
             pass
 
-env = simpy.Environment()
-p = env.process(chainer(env))
-env.run()
+#if this file is being run directly
+if __name__ == "__main__":
+    #run the simulation environment
+    env = simpy.Environment()
+    p = env.process(chainer(env))
+    env.run()
